fedd07bb9aa14bb43b8de3cfd304e9068085e54c
Simon Heiss <terentevsa002@gmail.com>
2026-06-09 14:50:24 +0300

    create migration 001_initial

---
+++ Data/Migrations/001_initial.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS Users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    telegram_id BIGINT UNIQUE NOT NULL,
+    registar_date DATE DEFAULT CURRENT_DATE,
+    last_active DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE IF NOT EXISTS Games (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES Users(id) ON DELETE CASCADE,
+    external_game_id BIGINT,
+    game_name VARCHAR(255) NOT NULL,
+    game_status INT NOT NULL,
+    user_rating INT,
+    note TEXT,
+    date_added DATE NOT NULL,
+    completion_date DATE
+);
