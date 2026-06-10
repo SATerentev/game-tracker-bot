a1bd9750ee6e0fb649069e2cc9548403b8dc8bf4
Simon Heiss <terentevsa002@gmail.com>
2026-06-10 17:42:36 +0300

    views update

--- Web/controller/AdminController.go
+++ Web/controller/AdminController.go
@@ -4,75 +4,75 @@ import (
 	"html/template"
 	"net/http"
 	"strconv"
-	"web-app/DTO"
+	"web-app/TemplatesData"
 	"web-app/service"
 )
 
+const baseTemplatePath = "templates/Base.html"
 const usersTemplatePath = "templates/Users.html"
 const userInfoTemplatePath = "templates/UserInfo.html"
 const errorTemplatePath = "templates/Error.html"
 
+const errorPage = "Error.html"
+const usersPage = "Users.html"
+const userInfoPage = "UserInfo.html"
+
 type AdminController struct {
-	adminService     service.AdminService
-	usersTemplate    template.Template
-	userInfoTemplate template.Template
-	errorTemplate    template.Template
-}
-
-type UsersPageData struct {
-	Users []DTO.User
-}
-
-type UserInfoPageData struct {
-	User  DTO.User
-	Games []DTO.Game
+	adminService service.AdminService
+	templates    *template.Template
 }
 
 func NewAdminController(service service.AdminService) AdminController {
-	usersTemplate, err := template.ParseFiles(usersTemplatePath)
+	templates, err := template.ParseFiles(
+		baseTemplatePath,
+		usersTemplatePath,
+		userInfoTemplatePath,
+		errorTemplatePath,
+	)
 	if err != nil {
 		panic(err)
 	}
-	userInfoTemplate, err := template.ParseFiles(userInfoTemplatePath)
-	if err != nil {
-		panic(err)
-	}
-	errorTemplate, err := template.ParseFiles(errorTemplatePath)
-	if err != nil {
-		panic(err)
-	}
+
 	return AdminController{
-		adminService:     service,
-		usersTemplate:    *usersTemplate,
-		userInfoTemplate: *userInfoTemplate,
-		errorTemplate:    *errorTemplate,
+		adminService: service,
+		templates:    templates,
 	}
 }
 
 func (c *AdminController) UsersHandler(w http.ResponseWriter, r *http.Request) {
 	users, err := c.adminService.GetAllUsers()
 	if err != nil {
-		panic(err)
+		c.templates.ExecuteTemplate(w, errorPage, TemplatesData.ErrorPageData{err.Error()})
+		return
 	}
-	data := UsersPageData{Users: users}
-	c.usersTemplate.Execute(w, data)
+
+	data := TemplatesData.UsersPageData{Users: users}
+	c.templates.ExecuteTemplate(w, usersPage, data)
 }
 
 func (c *AdminController) UserInfoHandler(w http.ResponseWriter, r *http.Request) {
 	userId, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil {
-		c.errorTemplate.Execute(w, err)
+		c.templates.ExecuteTemplate(w, errorPage, TemplatesData.ErrorPageData{err.Error()})
+		return
+	}
+
+	user, err := c.adminService.GetUserInfo(userId)
+	if err != nil {
+		c.templates.ExecuteTemplate(w, errorPage, TemplatesData.ErrorPageData{err.Error()})
+		return
 	}
 
 	userGames, err := c.adminService.GetAllUserGames(userId)
 	if err != nil {
-		c.errorTemplate.Execute(w, err)
+		c.templates.ExecuteTemplate(w, errorPage, TemplatesData.ErrorPageData{err.Error()})
+		return
 	}
-	user, err := c.adminService.GetUserInfo(userId)
-	data := UserInfoPageData{
+
+	data := TemplatesData.UserInfoPageData{
 		User:  user,
 		Games: userGames,
 	}
 
-	c.userInfoTemplate.Execute(w, data)
+	c.templates.ExecuteTemplate(w, userInfoPage, data)
 }
